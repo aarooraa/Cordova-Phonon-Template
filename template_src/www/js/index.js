1c6734c1a97a4debcae3bfb074c865d33a8c50bf
Ramanan Sambandam <tiruvarur.ramanan@gmail.com>
2018-01-07 09:09:20 +0530

    Template project structure created

---
+++ template_src/www/js/index.js
@@ -0,0 +1,32 @@
+phonon.options({
+    navigator: {
+        defaultPage: 'home',
+        animatePages: false,
+        enableBrowserBackButton: true
+    },
+
+    i18n: null
+});
+
+var app = phonon.navigator();
+
+app.on({ page: 'home', content: 'home.html' });
+
+app.on({ page: 'pageone', content: 'pageone.html' }, function (activity) {
+    activity.onReady(function () {
+
+    });
+});
+
+document.addEventListener('deviceready', function () {
+    app.start()
+
+    admob.banner.config({
+        id: admobid.banner,
+        isTesting: false,
+        autoShow: true,
+    });
+
+    admob.banner.prepare();
+});
+
